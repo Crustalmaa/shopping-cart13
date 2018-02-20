beac1f043f70948dad9998ad422c5c5efe4f2d75
unknown <Crustalmaa@gmail.com>
2018-02-20 15:53:04 -0800

    commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,5 +3,13 @@ function onSignIn(googleUser){
   $(".g-signin2").css("display", "none");
   $(".data").css("display", "block");
   $("#pic").attr('src',  profile.getImageUrl());
-  $("#email").text(profile.getEmail()); 
+  $("#email").text(profile.getEmail());
+}
+function signOut(){
+  var auth2 =  gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function(){
+    alert("you have successfully signed out");
+    $(".g-signin2").css("display", "block");
+    $(".data").css("display", "none");
+  });
 }
