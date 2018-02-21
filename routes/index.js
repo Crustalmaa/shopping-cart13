9831aecf8cb1c3eb14c94194d1e2f4fd3bede3ea
unknown <Crustalmaa@gmail.com>
2018-02-20 16:21:06 -0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/gmail', function(req, res, next){
+    res.render('gmailSignin.hbs', {title: 'gmail'});
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
